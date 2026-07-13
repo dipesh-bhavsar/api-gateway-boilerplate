21519919dfa4151c7aee7cfb83ff6249e5bf8ed7
Dipesh Bhavsar <dipesh.mywork@gmail.com>
2026-07-13 12:42:44 +0000

    Set up request ID middleware to reduce duplication

---
+++ src/middleware/requestId.js
@@ -0,0 +1,3 @@
+const{v4:uuidv4}=require('uuid');
+function requestIdMiddleware(req,res,next){req.id=req.headers['x-request-id']||uuidv4();res.setHeader('X-Request-Id',req.id);next();}
+module.exports={requestIdMiddleware};
